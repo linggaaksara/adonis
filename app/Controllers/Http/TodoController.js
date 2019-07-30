e98d571031887742c6ab3284dc4652f928a3884c
Lingga_Aksara46 <46749766+linggaaksara@users.noreply.github.com>
2019-07-30 08:11:17 +0700

    Add files via upload

---
+++ app/Controllers/Http/TodoController.js
@@ -0,0 +1,29 @@
+'use strict'
+
+class TodoController {
+    index({request, response, view}){
+        return view.render('crud.index')
+    }
+
+    page2({request, response, view}){
+        return view.render('crud.page2')
+    }
+
+    page3({request, response, view}){
+        return view.render('crud.page3')
+    }
+
+    page4({request, response, view}){
+        return view.render('crud.page4')
+    }
+
+    home({request, response, view}){
+        return view.render('crud.home')
+    }
+
+    login({request, response, view}){
+        return view.render('crud.login')
+    }
+}
+
+module.exports = TodoController
